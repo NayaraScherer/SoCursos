915cac77841e72ce23818cdd16fc9783224be15f
Nayara Scherer <nayara.scherer@bartdigital.com.br>
2024-02-21 13:12:21 -0400

    Initial commit

---
+++ src/programa/Main.java
@@ -0,0 +1,19 @@
+package programa;
+
+import socursos.Aluno;
+import socursos.Curso;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.printf("Qual o seu nome ?");
+        sc.next();
+
+        System.out.println("Qual o nome do curso que queres fazer?");
+        sc.next();
+
+    }
+}
