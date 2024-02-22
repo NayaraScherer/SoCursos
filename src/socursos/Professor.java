321872e1f042bc9408051de3cb488032dccba8f6
Nayara Scherer <nayara.scherer@bartdigital.com.br>
2024-02-22 14:56:03 -0400

    Initial commit

--- src/socursos/Professor.java
+++ src/socursos/Professor.java
@@ -5,4 +5,27 @@ public class Professor {
     public String emailProf;
     public String cursosMinistrados;
 
+    public String getNomeProf() {
+        return nomeProf;
+    }
+
+    public void setNomeProf(String nomeProf) {
+        this.nomeProf = nomeProf;
+    }
+
+    public String getEmailProf() {
+        return emailProf;
+    }
+
+    public void setEmailProf(String emailProf) {
+        this.emailProf = emailProf;
+    }
+
+    public String getCursosMinistrados() {
+        return cursosMinistrados;
+    }
+
+    public void setCursosMinistrados(String cursosMinistrados) {
+        this.cursosMinistrados = cursosMinistrados;
+    }
 }
