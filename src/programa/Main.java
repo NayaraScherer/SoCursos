0cc508b761b87eb01327e7315df8e50cf2a49b72
Nayara Scherer <nayara.scherer@bartdigital.com.br>
2024-02-22 14:56:03 -0400

    Initial commit

--- src/programa/Main.java
+++ src/programa/Main.java
@@ -2,6 +2,7 @@ package programa;
 
 import socursos.Aluno;
 import socursos.Curso;
+import socursos.Professor;
 
 import java.util.Scanner;
 
@@ -9,11 +10,50 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
 
-        System.out.printf("Qual o seu nome ?");
-        sc.next();
+        Curso curso=new Curso();
+        System.out.printf("Qual o nome do curso que queres fazer? ");
+        curso.setNomeCurso(sc.next());
+        System.out.println(curso.getNomeCurso());
 
-        System.out.println("Qual o nome do curso que queres fazer?");
-        sc.next();
+        curso.setInfoCurso("O curso de Java COMPLETO (Programação Orientada a Objetos + Projetos)");
+        System.out.println(curso.getInfoCurso());
+
+        curso.setNomeProf("Giovana Adorno");
+        System.out.println("Prof.(a): " + curso.getNomeProf());
+
+        curso.setValorCurso(299);
+        System.out.println("Valor do curso é  R$ "+curso.getValorCurso());
+
+        curso.setListAlunos("Nayara"+ " ,Daniel ");
+        System.out.println("Alunos: "+ curso.getListAlunos());
+
+        Aluno aluno=new Aluno();
+        aluno.setNomeAluno("Nayara" );
+        System.out.println("\nAluno(a): " + aluno.getNomeAluno());
+
+        aluno.setEmailAluno("nayara.scherer@bartdigital.com.br");
+        System.out.println("E-mail: "+aluno.getEmailAluno());
+
+        aluno.setTelefone("92 99999-9999");
+        System.out.println("Tel.: "+ aluno.getTelefone());
+
+        aluno.setCursosMatriculados("Java Completo");
+        System.out.println("Cursos Matriculados: "+ aluno.getCursosMatriculados());
+
+        Professor professor=new Professor();
+        professor.setNomeProf("Giovana Adorno");
+        System.out.println("\nProf.(a): "+ professor.getNomeProf());
+
+        professor.setEmailProf("giovana.adorno@bartdigital.com.br");
+        System.out.println("E-mail: "+ professor.getEmailProf());
+
+        professor.setCursosMinistrados("Java");
+        System.out.println("Cursos Ministrados: "+professor.getCursosMinistrados());
+
+
+
+
+
 
     }
 }
--- src/socursos/Aluno.java
+++ src/socursos/Aluno.java
@@ -4,8 +4,41 @@ public class Aluno {
     public String nomeAluno;
 
     public String emailAluno;
-    public int senha;
-    public String cursosMatriculados;
     public String telefone;
 
+    public String cursosMatriculados;
+
+
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public void setNomeAluno(String nomeAluno) {
+        this.nomeAluno = nomeAluno;
+    }
+
+    public String getEmailAluno() {
+        return emailAluno;
+    }
+
+    public void setEmailAluno(String emailAluno) {
+        this.emailAluno = emailAluno;
+    }
+
+
+    public String getCursosMatriculados() {
+        return cursosMatriculados;
+    }
+
+    public void setCursosMatriculados(String cursosMatriculados) {
+        this.cursosMatriculados = cursosMatriculados;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
